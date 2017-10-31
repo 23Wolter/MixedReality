c1a3497b6fea224a1b0c55278149ab6549ee12ab
23Wolter <wolter.nielsen@gmail.com>
2017-10-31 14:21:03 +0100

    mobile functionality

---
+++ Assets/MobilePopup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePopup : MonoBehaviour {
+
+    private Vector3 pos;
+    private Vector3 oldPos; 
+    private bool rotate;
+    private MobileRotation mobileRotation;
+    private bool mobileIsUp;
+
+    public float moveSpeed; 
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+        mobileIsUp = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1"))
+        {
+            if (!mobileIsUp)
+            {
+                MobileRotation.rotate = false;
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z), moveSpeed * Time.deltaTime); 
+                mobileIsUp = true; 
+            } else
+            {
+                MobileRotation.rotate = true;
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y - 2f, transform.position.z), moveSpeed * Time.deltaTime);
+                mobileIsUp = false; 
+            }
+        }
+	}
+}
---
+++ Assets/MobileRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileRotation : MonoBehaviour {
+
+    public GameObject camera;
+    public static bool rotate; 
+    public float rotateSpeed = 5f; 
+
+	// Use this for initialization
+	void Start () {
+        rotate = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotate)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, camera.transform.rotation, rotateSpeed * Time.deltaTime);
+        }
+	}
+}
