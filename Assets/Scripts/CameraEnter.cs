b64b09b4bf4935be99974f4cc688a753ec4d0299
23Wolter <wolter.nielsen@gmail.com>
2017-11-08 15:18:33 +0100

    added gun asset

---
+++ Assets/Scripts/CameraEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnter : MonoBehaviour {
+
+    public float rotateSpeed = 5f; 
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(transform.rotation);
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
+        transform.rotation = rotation;//Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+        Debug.Log(transform.rotation); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
